69efcbd8c3be7c98ad0107ded0a782ad81c0fd76
mohamed-eldeghiedy <eldegedym@gmail.com>
2025-06-28 00:08:54 +0300

    Add project files.

---
+++ Assignment02_c/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Assignment02_c
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( " Enter Number"
+            Console.ReadLine();
+        }
+    }
+}
