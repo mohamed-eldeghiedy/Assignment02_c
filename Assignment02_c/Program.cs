b2dd1d1c26aee78170fb8c727d9a7395104fe773
mohamed-eldeghiedy <eldegedym@gmail.com>
2025-06-29 03:24:03 +0300

    p1

--- Assignment02_c/Program.cs
+++ Assignment02_c/Program.cs
@@ -2,10 +2,23 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine( " Enter Number"
-            Console.ReadLine();
+
+            #region p1:Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+
+
+            Console.WriteLine(" Enter Number : ");
+            int number = int.Parse(Console.ReadLine()!);
+            if (number % 3 == 0 && number % 4 == 0)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+            #endregion
         }
     }
 }
