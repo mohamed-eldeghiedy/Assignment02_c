935ad8eede2feae11fe2f3ce2972a12c631c74f8
mohamed-eldeghiedy <eldegedym@gmail.com>
2025-06-29 13:41:37 +0300

    p7

--- Assignment02_c/Program.cs
+++ Assignment02_c/Program.cs
@@ -91,16 +91,29 @@
 
             #region p6:Write a program that allows the user to insert an integer then print all numbers between 1 to that number.
 
-            Console.WriteLine(" Enter Number : ");
-            int input = int.Parse(Console.ReadLine()!);
-            for (int i = 1; i <= input; i++) 
-            {
+            //Console.WriteLine(" Enter Number : ");
+            //int input = int.Parse(Console.ReadLine()!);
+            //for (int i = 1; i <= input; i++) 
+            //{
 
-                Console.Write(i + " , ");
-            }
+            //    Console.Write(i + " , ");
+            //}
+            //Console.WriteLine();
+
+
+            #endregion
+
+
+            #region p7:Write a program that allows the user to insert an integer then print a multiplication table up to 12.
+
+            Console.WriteLine(" Enter Number :");
+            int num_mlt = int.Parse(Console.ReadLine()!);
             Console.WriteLine();
 
-
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine(i*num_mlt);
+            }
             #endregion
         }
     }
